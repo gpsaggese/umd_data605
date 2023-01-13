0aa979fe3279c3ff1343a715c6e30c61cb3c1929
saggese <saggese@gmail.com>
2023-01-13 03:54:33 -0500

    Checkpoint

--- dev_scripts/setenv.sh
+++ dev_scripts/setenv.sh
@@ -1,2 +1,5 @@
 alias gs="git status"
 alias gd="git diff"
+
+export GIT_REPO=$(pwd -P)
+echo "GIT_REPO=$GIT_REPO"
--- tutorials/tutorial_postgres/docker_bash.sh
+++ tutorials/tutorial_postgres/docker_bash.sh
@@ -2,6 +2,7 @@
 
 IMAGE_NAME=umd_data605_postgres
 REPO_NAME=gpsaggese
+
 CONTAINER_NAME=$IMAGE_NAME
 docker image ls $REPO_NAME/$IMAGE_NAME
 docker run --rm -ti --name $CONTAINER_NAME -p 8888:8888 -p 5432:5432 -v $(pwd):/data $REPO_NAME/$IMAGE_NAME
