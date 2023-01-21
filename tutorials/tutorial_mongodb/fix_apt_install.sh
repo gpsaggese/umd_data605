a320133a02703060f12892a5c19fccf822997001
saggese <saggese@gmail.com>
2023-01-21 13:03:54 -0500

    Checkpoint

--- tutorials/tutorial_mongodb/fix_apt_install.sh
+++ tutorials/tutorial_mongodb/fix_apt_install.sh
@@ -1,4 +1,4 @@
-#!/bin/use/env bash -xe
+#!/bin/bash -xe
 
 # From https://askubuntu.com/questions/1385440/ubuntu-sudo-apt-get-update-404-not-found-problem
 DIR=/tmp/fix_apt_install
@@ -22,10 +22,10 @@ deb http://archive.canonical.com/ubuntu focal partner
 deb-src http://archive.canonical.com/ubuntu focal partner
 EOF
 
-sudo sed -i "s/focal/$(lsb_release -c -s)/" $DIR/sources.list
-sudo rm /etc/apt/sources.list
-sudo cp $DIR/sources.list /etc/apt/sources.list
+#sed -i "s/focal/$(lsb_release -c -s)/" $DIR/sources.list
+rm /etc/apt/sources.list
+cp $DIR/sources.list /etc/apt/sources.list
 
-sudo mv /etc/apt/sources.list.d/* ~/solution
+#mv /etc/apt/sources.list.d/* $DIR || true
 
-sudo apt update
+apt update
