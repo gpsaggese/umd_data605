bff5531cca0a80e0c777ea0c5cbf14325504bae2
saggese <saggese@gmail.com>
2023-02-03 10:32:32 -0500

    Remove old dir

--- dev_scripts/rsync_sorrentum_sandbox.sh
+++ dev_scripts/rsync_sorrentum_sandbox.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
+ROOT_DIR=/Users/saggese/src/cmamp2
+
 DIR=sorrentum_sandbox
-SRC_DIR=/Users/saggese/src/cmamp2/$DIR
+SRC_DIR=$ROOT_DIR/$DIR
 #(cd $SRC_DIR; invoke git_clean)
 rm -rf $DIR
 rsync -arhv $SRC_DIR/ $DIR/
--- sorrentum_sandbox/sorrentum_data_node/docker_exec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-IMAGE_NAME="airflow_cont"
-
-CONTAINER_ID=$(docker container ls | grep $IMAGE_NAME | awk '{print $1}')
-docker exec -it $CONTAINER_ID bash
