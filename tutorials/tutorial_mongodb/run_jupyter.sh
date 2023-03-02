c7af2f2a06cde6682eb48b922fc3e91fd4068705
saggese <saggese@gmail.com>
2023-03-02 17:06:52 -0500

    Update slides

--- tutorials/tutorial_mongodb/run_jupyter.sh
+++ tutorials/tutorial_mongodb/run_jupyter.sh
@@ -2,5 +2,5 @@
 
 /data/run_mongo.sh
 # Enable vim bindings.
-#jupyter nbextension enable vim_binding/vim_binding
+jupyter nbextension enable vim_binding/vim_binding
 jupyter-notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --allow-root
