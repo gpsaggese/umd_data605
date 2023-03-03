b48f67f10c18fe7c4bdcc2cfe40b9164106f7fd3
saggese <saggese@gmail.com>
2023-03-02 19:58:58 -0500

    Disable vim bindings

--- tutorials/tutorial_mongodb/run_jupyter.sh
+++ tutorials/tutorial_mongodb/run_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-/data/run_mongo.sh
+/data/run_mongo.sh || true
 # Enable vim bindings.
-jupyter nbextension enable vim_binding/vim_binding
+#jupyter nbextension enable vim_binding/vim_binding
 jupyter-notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --allow-root
