1fd79551c71eee8e530f1e49ebbadb8e19d3d38f
GP Saggese <saggese@gmail.com>
2024-07-22 18:49:16 -0400

    Update

---
+++ tutorials/tutorial_llms/tutorial_pyllms.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import llms
+
+# +
+model = llms.init('gpt-4')
+result = model.complete("what is 5+5")
+
+print(result.text)
+# -
+
+
+
+
+
+
